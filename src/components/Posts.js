717674c072ef4d0ac83d2ba5d57e16c030a80224
Eugénie <eugenie.b24@hotmail.fr>
2024-09-01 11:07:45 +0100

    Add Posts display

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,8 +5,26 @@ export default function Posts() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    console.log("Load Posts");
+    getPosts()
+      .then((result) => {
+        setPosts(result.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }, []);
 
-  return <></>;
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
